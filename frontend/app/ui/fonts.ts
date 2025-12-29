70af0825163cafaaaef7b5a949c12fe0e50e00f4
ParisaAskarian <pskryan@gmail.com>
2025-12-30 00:17:29 +0330

    add vazirmatn font

--- frontend/app/ui/fonts.ts
+++ frontend/app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter, Lusitana } from 'next/font/google';
+import { Vazirmatn } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
@@ -7,3 +8,9 @@ export const lusitana700 = Lusitana( { weight: '700'});
 export const lusitana400 = Lusitana( { weight: '400'});
 
 export const lusitana = Lusitana( { weight: '400'});
+
+export const vazirmatn = Vazirmatn({
+  subsets: ["arabic"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
